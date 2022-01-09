953deb7b500feebd367335760c9bbf37a953cbbe
Gary A. Newsome <garyanewsome@gmail.com>
2022-01-09 12:01:07 -0500

    fix zsh add obsidian backup

---
+++ scripts/obsidian_backup.sh
@@ -0,0 +1,13 @@
+cd ~/Github/obsidian-vault
+
+# CHANGES_EXIST="$(git status --porcelain | sc -l)"
+CHANGES_EXIST="$(git status --porcelain=v1  | wc -l)"
+echo $CHANGES_EXIST
+
+if [ "$CHANGES_EXIST" -eq 0 ]; then
+  exit 0
+fi
+
+DATE=`date +%Y-%m-%d_%H:%M:S`
+
+ git pull && git add . &&  git commit -q -m"Auto Sync: ($DATE)" && git push
